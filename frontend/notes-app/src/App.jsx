5548118dd45e526840e8afdf1572a45e97ba048f
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-05 15:19:57 -0600

    added routes

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,9 +1,22 @@
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />}></Route>
+      <Route path='/login' exact element={<Login />}></Route>
+      <Route path='/signup' exact element={<SignUp />}></Route>
+    </Routes>
+  </Router>
+)
 
 function App() {
   return (
     <div>
-      <Home />
+      {routes}
     </div>
   )
 } 
