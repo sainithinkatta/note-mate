4efe1c264aec7e64f3fa502e0ba1518668060da7
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-05 15:44:58 -0600

    updated title and icon

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar () {
     return (
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
             <h2 className="text-xl font-medium text-black py-2">
-                Notes
+                NoteMate
             </h2>
         </div>
     )
