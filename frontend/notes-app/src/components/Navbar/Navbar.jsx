5fe6fa6894947967066c2442055c59836ece3a32
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-05 21:16:22 -0600

    added SearchBar component

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
+import SearchBar from "../SearchBar/SearchBar"
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+    const [searchQuery, setSearchQuery] = useState("");
     const navigate = useNavigate();
 
     function onLogout() {
         navigate("/login");
     }
 
+    function handleSearch () {
+        // handle search logic...
+    }
+
+    function onClearSearch () {
+        setSearchQuery("");
+    }
+
     return (
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
             <h2 className="text-xl font-medium text-black py-2">NoteMate</h2>
 
+            <SearchBar 
+                value={searchQuery}
+                onChange={({ target }) => {
+                    setSearchQuery(target.value);
+                }}
+                handleSearch={handleSearch}
+                onClearSearch={onClearSearch}
+            />
+            
             <ProfileInfo onLogout={onLogout} />
         </div>
     );
