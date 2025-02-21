f5c15123c462f3f431a29c577c7e5befe6c68e22
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-21 14:40:33 -0600

    integrtaed create account API

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -3,8 +3,12 @@ import PasswordInput from "../../components/Input/PasswordInput";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { validateEmail } from "../../utils/helper";
+import  axiosInstance from "../../utils/axiosInstance";
+
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
+    const navigate = useNavigate();
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -30,7 +34,28 @@ function SignUp() {
 
         setError("");
 
-        // SignUp API call logic
+        try {
+            const response = await axiosInstance.post("/create-account", {
+                fullName: name,
+                email: email,
+                password: password
+            })
+
+            if (response.data && response.data.accessToken) {
+                localStorage.setItem("token", response.data.accessToken)
+                
+                navigate('/dashboard')
+            }
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                setError(error.response.data.message)
+            } else {
+                setError("An unexpected error occurred. Please try again later.")
+            }
+            
+        }
+
+        
     }
 
     return (
