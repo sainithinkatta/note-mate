916f681e5112b0eba8d60ba19a9395d98aa7b596
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-21 17:08:30 -0600

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
