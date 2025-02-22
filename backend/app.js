ffac456cb742550c5feac46f8a6454fba37df21f
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-21 19:33:33 -0600

    deployment changes

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://note-mate-two.vercel.app/' }));
 
 // Database Connection
 connectDB();
--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8000/api'
+export const BASE_URL = 'https://note-mate-two.vercel.app/api'
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
