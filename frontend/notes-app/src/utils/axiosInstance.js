0c7d507cf3d6d40e3ca954e32ff96d006fb79a80
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-23 22:53:37 -0600

    deployment path change

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://note-mate-two.vercel.app/api'
+export const BASE_URL = 'https://notemate-backend.vercel.app/api'
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
