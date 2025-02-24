f3c915243a72a93b5e77b11718acbd41425cb74b
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-23 23:08:57 -0600

    added allowHeaders

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cors({
     origin: ['https://notemate-app.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
