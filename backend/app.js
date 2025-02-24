37e46617edffca461668d6cea8b554ac13286387
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-23 22:55:42 -0600

    Updated CORS policy

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['https://note-mate-two.vercel.app', 'http://localhost:5173'],
+    origin: ['https://notemate-app.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
