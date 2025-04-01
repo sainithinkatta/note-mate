0f8fd9f63bcb72764fd0d78c3c2210cd71fb896a
sainithinkatta <sainithinkatta24@gmail.com>
2025-04-01 01:25:36 -0500

    vercel changes

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8080/api/'
+// Uncomment while using local development.
+// export const BASE_URL = 'http://localhost:8080/api/'
+export const BASE_URL = 'https://note-mate-backend.vercel.app/api/'
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
